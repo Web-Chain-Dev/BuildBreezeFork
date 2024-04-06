010f546ffbac7c7377bf3b5fe3f11e8a24f6b6f9
Web-Chain-Dev <frolov.milan@gmail.com>
2024-04-06 15:54:37 +0300

    remove static buyerId

--- components/home/PricingPlan.tsx
+++ components/home/PricingPlan.tsx
@@ -21,7 +21,6 @@ const PricingPlan = ({buyerId}: PricingPlanProps) => {
           plan: "basic",
           amount: 25,
           buyerId: buyerId
-          // buyerId: "66108f92408c1fbb0fe29286"
         }
     
         await checkoutPlan(transaction)
