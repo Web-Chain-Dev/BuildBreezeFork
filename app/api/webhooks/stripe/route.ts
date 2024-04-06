7eb7fc8f0a6898e4df88f18506954c54ac8ebbca
Web-Chain-Dev <frolov.milan@gmail.com>
2024-04-06 14:11:19 +0300

    new route test

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,29 +1,30 @@
-// /* eslint-disable camelcase */
-// import { createTransaction } from "@/lib/actions/transaction.action";
-// import { NextResponse } from "next/server";
-// import stripe from "stripe";
+/* eslint-disable camelcase */
+import { createTransaction } from "@/lib/actions/transaction.action";
+import { NextResponse } from "next/server";
+import stripe from "stripe";
 
-// export async function POST(request: Request) {
+export async function POST(request: Request) {
   
-//   const body = await request.text();
+  const body = await request.text();
 
-//   const sig = request.headers.get("stripe-signature") as string;
-//   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+  const sig = request.headers.get("stripe-signature") as string;
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-//   let event;
+  let event;
 
-//   try {
-//     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-//   } catch (err) {
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
     
-//     return NextResponse.json({ message: "Webhook error", error: err });
+    return NextResponse.json({ message: "Webhook error", error: err });
   
-//   }
+  }
+}
 
-//   // Get the ID and type
-//   const eventType = event.type;
+  // Get the ID and type
+  // const eventType = event.type;
 
-//   // CREATE
+  // CREATE
 //   if (eventType === "checkout.session.completed") {
 //     const { id, amount_total, metadata } = event.data.object;
 
