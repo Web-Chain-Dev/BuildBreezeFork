78237b7e697ec201643742ba3ae29c6b1500948a
Web-Chain-Dev <frolov.milan@gmail.com>
2024-04-06 14:00:08 +0300

    route

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 import stripe from "stripe";
 
 export async function POST(request: Request) {
+  
   const body = await request.text();
 
   const sig = request.headers.get("stripe-signature") as string;
@@ -14,7 +15,9 @@ export async function POST(request: Request) {
   try {
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
+    
     return NextResponse.json({ message: "Webhook error", error: err });
+  
   }
 
   // Get the ID and type
