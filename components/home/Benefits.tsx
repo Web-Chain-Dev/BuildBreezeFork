8fa5bc985989daa446a13889be76f0de57239d3b
Web-Chain-Dev <frolov.milan@gmail.com>
2024-04-09 01:38:14 +0300

    boring

--- components/home/Benefits.tsx
+++ components/home/Benefits.tsx
@@ -10,7 +10,7 @@ const Benefits = () => {
       // style={{ backgroundImage: `url(/assets/bluepurple14.jpg)` }}
     >
       <h3 className="text-purple-500 text-center text-5xl md:text-6xl lg:text-7xl font-semibold ">
-        Stop wasting time on the mundane
+        Stop wasting time on the boring and mundane
       </h3>
       <RealInfoCard
         image="/assets/bluepurple19.jpg"
